e46935cb3fdd22003591b7e61eb82815537a56df
Paul Moesher <36650592+paul-moesher@users.noreply.github.com>
2019-05-21 10:56:46 -0400

    Add files via upload

---
+++ palindrome/Palindrome/Palindrome/Palindrome.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    public class Palindrome
+    {
+        public static bool IsPalindrome(string userInput)
+        {
+            try
+            {
+                if (userInput.Length <= 1)
+                    return true;
+                else
+                {
+                    if (userInput[0] != userInput[userInput.Length - 1])
+                        return false;
+                    else
+                        return IsPalindrome(userInput.Substring(1, userInput.Length - 2));
+                }
+            } catch (Exception ex)
+            {
+                Console.WriteLine("Please contact System Administrator");
+                return false;
+            }
+        }
+    }
+}
+
+
+
---
+++ palindrome/Palindrome/Palindrome/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            bool result;
+
+            try
+            {
+                Console.Write("Input a word to test if its a palindrome: ");
+                userInput = Console.ReadLine();
+
+                result = Palindrome.IsPalindrome(userInput);
+                if (result == true)
+                {
+                    Console.WriteLine("The word is a Palindrome. \n");
+                }
+                else
+                {
+                    Console.WriteLine("The word is NOT a Palindrome. \n");
+                }
+
+                do
+                {
+                    Console.Write("Enter another word to test or type Exit to end: ");
+                    userInput = Console.ReadLine();
+
+                    if (userInput == "Exit")
+                    {
+                        break;
+                    }
+
+                    result = Palindrome.IsPalindrome(userInput);
+                    if (result == true)
+                    {
+                        Console.WriteLine("The word is a Palindrome. Press enter to contiune. \n");
+                    }
+                    else
+                    {
+                        Console.WriteLine("The word is NOT a Palindrome. Press enter to contiune. \n");
+                    }
+                } while (userInput != "Exit");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Please contact System Administrator");
+            }
+        }
+    }
+}
